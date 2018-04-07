5c245d767ca9f70badcf9824b00ffed704e71536
Spectre <antoine@doussaud.org>
2018-04-08 00:10:28 +0200

    test radio

--- Radio_esp32_arduino.ino
+++ Radio_esp32_arduino.ino
@@ -9,7 +9,7 @@
 #include <FS.h>
 #include <SD.h>
 
-#include "audio.hpp"
+// #include "audio.hpp"
 
 #define NEOPIXEL_PIN 5
 
@@ -20,30 +20,26 @@
 
 #define ADC_CS 21
 
-#define NRF24_CE 26
-#define NRF24_CS 26
+#define NRF24_CE 5
+#define NRF24_CS 5
 #define PAYLOAD_SIZE 16
 
 #define RADIO_PE1 13
 #define RADIO_PE2 12
 
 
-//#define USE_RADIO
-#define USE_TFT
-#define USE_LED
+#define USE_RADIO
+// #define USE_TFT
+// #define USE_LED
 //#define USE_INT_ADC
-#define USE_EXT_ADC
-#define USE_SD
+// #define USE_EXT_ADC
+// #define USE_SD
 
 
 uint8_t addresses[5] = {0xe7,0xe7,0xe7,0xe7,0xe7};
 // uint8_t data[PAYLOAD_SIZE];
 
 uint16_t adc_value[8];
-
-#ifdef USE_RADIO
-	RF24 radio(NRF24_CE, NRF24_CS);
-#endif
 
 #ifdef USE_TFT
 	Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
@@ -58,8 +54,16 @@ uint16_t adc_value[8];
 	Adafruit_MCP3008 adc;
 #endif
 
+#ifdef USE_RADIO
+	RF24 radio(NRF24_CE, NRF24_CS);
+#endif
+
 void setup() {
 	Serial.begin(115200);
+
+	SPI.setFrequency(200000);
+	// SPI.setBitOrder(MSBFIRST);
+	// SPI.setDataMode(0);
 
 	#ifdef USE_LED
 		leds.begin();
@@ -68,41 +72,8 @@ void setup() {
 		leds.show();
 	#endif
 
-	#ifdef USE_RADIO
-		pinMode(RADIO_PE1, OUTPUT);
-		pinMode(RADIO_PE2, OUTPUT);
-		digitalWrite(RADIO_PE1, HIGH); // nrf24 (pE1 high, PE2 low)
-		digitalWrite(RADIO_PE2, LOW);
-
-		//SPI.setFrequency(2000000);
-		//SPI.setBitOrder(MSBFIRST);
-		//SPI.setDataMode(0);
-
-		if (radio.begin())
-			Serial.println("OK!");
-		else
-			Serial.println("Radio error init");
-
-		Serial.println("\n");
-
-		radio.setPALevel(RF24_PA_LOW);
-		radio.setChannel(0);
-		radio.setAddressWidth(5);
-		radio.setDataRate(RF24_1MBPS);
-		radio.setCRCLength(RF24_CRC_16);
-		//radio.setAutoAck(false);
-		radio.setRetries(15, 7);
-		radio.setPayloadSize(PAYLOAD_SIZE);
-		radio.openWritingPipe(addresses);
-
-		radio.powerUp();
-		radio.printDetails();
-	#endif
-
 	#ifdef USE_TFT
 		tft.begin();
-		tft.setRotation(1);
-		tft.fillScreen(ILI9341_BLACK);
 		ts.begin();
 	#endif
 
@@ -134,11 +105,50 @@ void setup() {
 			}
 		}
 	#endif
+
+	#ifdef USE_TFT
+		tft.setRotation(1);
+		tft.fillScreen(ILI9341_BLACK);
+	#endif
+
+	#ifdef USE_RADIO
+		pinMode(RADIO_PE1, OUTPUT);
+		pinMode(RADIO_PE2, OUTPUT);
+		digitalWrite(RADIO_PE1, HIGH); // nrf24 (pE1 high, PE2 low)
+		digitalWrite(RADIO_PE2, LOW);
+
+		//delay(100);
+
+		// SPI.setFrequency(20000000);
+		// SPI.setBitOrder(MSBFIRST);
+		// SPI.setDataMode(0);
+
+		if (radio.begin())
+			Serial.println("OK!");
+		else {
+			Serial.println("Radio error init");
+			//while (1);
+		}
+		Serial.println("\n");
+
+		radio.setPALevel(RF24_PA_LOW);
+		radio.setChannel(0);
+		radio.setAddressWidth(5);
+		radio.setDataRate(RF24_1MBPS);
+		radio.setCRCLength(RF24_CRC_16);
+		//radio.setAutoAck(false);
+		radio.setRetries(15, 7);
+		radio.setPayloadSize(PAYLOAD_SIZE);
+		radio.openWritingPipe(addresses);
+
+		radio.powerUp();
+		radio.printDetails();
+	#endif
 }
 
 void loop() {
 	#ifdef USE_TFT
-		tft.fillScreen(ILI9341_BLACK);
+		//tft.fillScreen(ILI9341_BLUE);
 		tft.setCursor(0, 0);
 		tft.setTextColor(ILI9341_WHITE,0);
 		tft.setTextSize(2);
@@ -165,7 +175,8 @@ void loop() {
 			tft.print("  [");
 			tft.print(chan);
 			tft.print("] = ");
-			tft.println(adc_value[chan]);
+			tft.print(adc_value[chan]);
+			tft.println("   ");
 		}
 
 		tft.println("\nTouchScreen:");
@@ -176,25 +187,41 @@ void loop() {
 			// Serial.print("x = "); Serial.println(p.x);
 			// Serial.print("y = "); Serial.println(p.y);
 		} else {
-			tft.println("  x:");
-			tft.println("  y:");
+			tft.println("  x:       ");
+			tft.println("  y:       ");
 		}
 	#endif
 
 	#ifdef USE_LED
 		leds.setPixelColor(0, leds.Color(
 			map(adc_value[0], 0, 1023, 0, 255),
-			map(adc_value[1], 0, 1023, 0, 255),
-			map(adc_value[2], 0, 1023, 0, 255)));
-			leds.show();
+			0,
+			0
+		));
+		leds.setPixelColor(1, leds.Color(
+			0,
+			map(adc_value[0], 0, 1023, 0, 255),
+			0
+		));
+		leds.setPixelColor(2, leds.Color(
+			0,
+			0,
+			map(adc_value[0], 0, 1023, 0, 255)
+		));
+		leds.setPixelColor(3, leds.Color(
+			map(adc_value[0], 0, 1023, 0, 255),
+			map(adc_value[0], 0, 1023, 0, 255),
+			0
+		));
+		leds.show();
 	#endif
 
-	#ifdef USE_RADIO
-		if (radio.write(adc_value, PAYLOAD_SIZE))
-			Serial.print("s\n");
-		else
-			Serial.print("f\n");
-	#endif
+	// #ifdef USE_RADIO
+	// 	if (radio.write(adc_value, PAYLOAD_SIZE))
+	// 		Serial.print("Radio send success\n");
+	// 	else
+	// 		Serial.print("Radio send failed\n");
+	// #endif
 
-	delay(2000);
+	delay(10);
 }
